c44b988914361adfda0cc88d6625c54339d9c3f0
Michael <michaelpspore@gmail.com>
2021-03-09 13:13:24 -0600

    footer setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import About from './components/About';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <main>
       <About />
       </main>
+      <Footer />
     </div>
   );
 }
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,7 +4,7 @@ import { Navbar, Nav } from 'react-bootstrap';
 function Navigation() {
     return (
       <Navbar  className="navbar my-4">
-          <Nav className="flex-right justify-content-around">
+          <Nav className="flex-wrap justify-content-around">
             <Nav.Link href="#about" className="link-style" >About Me</Nav.Link>
             <Nav.Link href="#portfolio" className="link-style ">Portfolio</Nav.Link>
             <Nav.Link href="#contact" className="link-style ">Contact Me</Nav.Link>
