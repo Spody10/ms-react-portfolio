b6202c005848577a09e3d1dcab29c9cd8e5163ff
Michael <michaelpspore@gmail.com>
2021-04-05 11:32:12 -0500

    updated about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -13,7 +13,7 @@ function About() {
                 <article className="pt-4 px-3 bio-text">
                 <p>I recently obtained a certificate as a Full Stack Developer from Vanderbilt University.</p> 
                 <p>I have both front end and back end skills including, but not limited to, HTML, CSS, Javascript, MySQL, and MERN. See resume link above for full list of skills developed.</p> 
-                <p>My previous work in...gives me an edge over some people due to...</p> 
+                <p>My previous work experience in sales and retail gives me an edge over some people becuase I'm able to probe for solutions to people's problems easily.  </p> 
                 <p>Outside of work, I like to spend time with family and friends, go fishing, and playing/watching sports.</p>
                 <p>The portfolio link above will give you examples of some of the work I've recently done. </p>
                 </article>
