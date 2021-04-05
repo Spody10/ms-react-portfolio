c6c67c077917098c2d0c60285c44f3faf3bd4186
Michael <michaelpspore@gmail.com>
2021-04-04 21:37:55 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import Header from './components/Header';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
+import Contact from './components/Contact'; 
+
 import Resume from './components/Resume';
 import Footer from './components/Footer';
 import './App.css';
